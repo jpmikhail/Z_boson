a94d74742c1804d93d0215cc83b98cbc5477191b
John Mikhail <39749907+jpmikhail@users.noreply.github.com>
2023-01-28 17:10:37 -0500

    Add files via upload

--- audio_processing.py
+++ audio_processing.py
@@ -13,14 +13,21 @@ def waveform_to_Fourier(waveform, dt):
 	return (norm_amplitudes, frequencies)
 	
 # function to find the union of the most significant frequencies for two different waveforms. freq_cutoff defines the minimized normalized amplitude to be considered in the union
-def significant_frequencies(waves_tuple, dt_tuple, freq_cutoff = 0.05):
+def significant_frequencies(waves_tuple, dt, freq_cutoff = 0.05):
 	n = len(waves_tuple) # should be 2 for now
 	n_amp            = [None for _ in range(n)]
-	freq             = [None for _ in range(n)]
 	inds_significant = [None for _ in range(n)]
 	for i in range(n):
-		(n_amp[i], freq[i]) = waveform_to_Fourier(waves_tuple[i], dt_tuple[i])
-		inds_significant[i] = n_amp[i] > freq_cutoff
+        # if first iteration, get frequency variable freq. Otherwise, freq should be the same
+        if i == 0:
+            (n_amp[i], freq) = waveform_to_Fourier(waves_tuple[i], dt)
+        else:
+            (n_amp[i], _) = waveform_to_Fourier(waves_tuple[i], dt)
+		inds_significant[i] = n_amp[i] > freq_cutoff 
 	
-	
-	
+	# now want the indices which are significant for any of the waves
+    inds = inds_significant[0]
+    for i in range(1, n):
+        inds = np.logical_or(inds, inds_significant[i])
+        
+    freq_significant = freq[inds]
