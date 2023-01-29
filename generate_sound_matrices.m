610020034d20f5e409049fa6ed9fa035734b31c2
John Mikhail <39749907+jpmikhail@users.noreply.github.com>
2023-01-29 08:56:15 -0500

    Add files via upload

--- generate_sound_matrices.m
+++ generate_sound_matrices.m
@@ -1,36 +1,30 @@
-%% Matlab Code
-% freqs = [300 400 500 600];
-% freqs = [220.00 246.94 261.63 293.66 329.63 349.23 392.00];
-% freqs = [261.63 392.00 329.63 349.23 392.00 0 0];
-f1 = 300;
-f2 = 250;
-f3 = 400;
-f4 = 430;
-% freqs = [f1 f1*1.25 f2 f2*1.25 f3 f3*1.25 f4 f4*1.25];
-freqs = [350 350*1.1224 370 f2*1.25 f3 f3*1.25 f4 f4*1.25];
-% amps = [9 1 1 0 3 4 5;
-%         3 2 6 1 0 0 0;
-%         0 9 1 2 6 6 0];
-% amps = [0 9 1 0 8 0 0;
-%         6 0 3 0 0 4 0;
-%         1 0 0 0 2 0 8];
-% amps = [0 6 1 0 8 0 3];
-amps = [9 9 0 0 0 0 0 0];
-amps = amps/10;
-R = height(amps);
-N = width(amps);
-durs = 1;
-% Fs = 22050;
-Fs = 48000;
+%% Generate a random sound matrix
+
+freqs = [220.00 246.94 261.63 293.66 329.63 349.23 392.00]*(2^(1/12))^4;
+amplitudes = randi(10, 10, 7);
+for i = 1:10
+    for j = 1:7
+        amplitudes(i, j) = amplitudes(i, j)*(amplitudes(i, j) > 7);
+    end
+end
+amplitudes = amplitudes/10;
+%% Convert the matrix to a sound file
+
+n_durations = height(amplitudes);
+n_notes = width(amplitudes);
+durations = 0.4; % [s]
+Fs = 48000; % sampling rate [Hz]
 t=0:1/Fs:1;
 x = [];
-for rr = 1:R
- ttemp = 0:1/Fs:durs;
- xtemp = zeros(size(ttemp));
- for ii = 1:N
-  xtemp = xtemp + amps(rr,ii)*sin(2*pi*freqs(ii)*ttemp);
+for dur_num = 1:n_durations
+ time = 0:1/Fs:durations;
+ waveform = zeros(size(time));
+ for note_num = 1:n_notes
+  waveform = waveform ...
+      + amplitudes(dur_num,note_num)*sin(2*pi*freqs(note_num)*time);
  end
- x = [x xtemp];
+ x = [x waveform];
 end
 soundsc(x, Fs);
-% audiowrite('A.mp4', x, Fs);
+audiowrite('AB1.0.mp4', x, Fs);
+writematrix(amplitudes, 'AB1.0.csv');
